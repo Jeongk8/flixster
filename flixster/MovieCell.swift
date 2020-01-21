1042483b00c109b3ebd5764de37f75ffa569714c
Jeong Kim <jeongk8@uci.edu>
2020-01-20 18:05:37 -0800

    Initial Commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Jeong Kim on 1/19/20.
+//  Copyright © 2020 Jeong Kim. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
